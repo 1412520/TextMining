d16e85eaf2ceaf44ff19d18d88e4c2f3dbdb8dee
Nguyễn Hoàng Thi <1412520@student.hcmus.edu.vn>
2018-03-24 12:56:41 +0700

    add vector class and compute similarity method

---
+++ Homework1/Homework1/Vector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public class Vector
+    {
+        public List<float> Value { get; set; }
+
+        public Vector(List<float> value)
+        {
+            Value = new List<float>(value);
+        }
+
+        public double GetSimilarityMeasure(Vector vector)
+        {
+            if (Value.Count != vector.Value.Count)
+                return 0;
+            double squareSum = 0;
+            for (int i=0; i<Value.Count; i++)
+            {
+                squareSum += Math.Pow((Value[i] - vector.Value[i]), 2);
+            }
+            return Math.Sqrt(squareSum);
+        }
+    }
+}
