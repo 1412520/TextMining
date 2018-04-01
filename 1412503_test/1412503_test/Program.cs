bf6e1755f3d4c868e5e5a7ab8d5fcea597464649
thaonguyen1096 <thaonguyen1096@gmail.com>
2018-04-01 18:58:26 +0700

    1412503 test

---
+++ 1412503_test/1412503_test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1412503_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileUtils.preprocessDirectory("../../inputFileTest", "../../outputFile/ouputFileTotal.txt");
+            Bow_tfidf.CreateFeatureList("../../outputFile/ouputFileTotal.txt", "../../inputFile/stop-words.txt", "../../outputFile/featureList.txt");
+            //Bow_tfidf.BoW_tfidf("../../outputFile/ouputFileTotal.txt", "../../outputFile/Bow_tfidf.txt", "../../outputFile/featureList.txt", "../../inputFile/round.txt", "../../inputFile/stop-words.txt");
+
+            List<Vector> test = Vector.VectoriseStandardInputFile("../../outputFile/ouputFileTotal.txt", "../../outputFile/featureList.txt", "../../inputFile/stop-words.txt");
+            
+            //foreach (var item in test)
+            //{
+            //     Console.WriteLine(item.TextValue.Text);
+            //    foreach (var item1 in item.Value)
+            //    {
+            //        Console.WriteLine(item1);
+            //    }
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ 1412503_test/1412503_test/Vector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1412503_test
+{
+    public class Vector
+    {
+        public List<double> Value { get; set; }
+        public Document TextValue { get; set; }
+
+        public Vector()
+        {
+            Value = new List<double>();
+        }
+
+        public Vector(List<double> value)
+        {
+            Value = new List<double>(value);
+        }
+
+        public Vector(string vector)
+        {
+            Value = new List<double>();
+            var numbers = vector.TrimEnd().Split(' ');
+            foreach (var number in numbers)
+            {
+                Value.Add(double.Parse(number));
+            }
+
+        }
+
+        public static Vector VectoriseStandardInput(string standardInput, string baseFile)
+        {
+            var featuresIdf = Bow_tfidf.GetFeaturesIdf(baseFile);
+            var result = new Vector();
+            if (featuresIdf.Count > 0)
+            {
+                var features = featuresIdf.Keys.ToArray();
+                var document = new Document(standardInput);
+                result.TextValue = document;
+                foreach (var feature in features)
+                {
+                    if (document.Contains(feature))
+                    {
+                        var tf_idf = 1.0 * document.getFrequency(feature) / document.getMaxFrequency() * featuresIdf[feature];
+                        result.Value.Add(tf_idf);
+                    }
+                    else
+                        result.Value.Add(0);
+                }
+            }
+            return result;
+        }
+
+        public static List<Vector> VectoriseStandardInputFile(string inputFile, string baseFile, string stopwordFile)
+        {
+            List<string> processText = FileUtils.processFileContent(inputFile, stopwordFile);
+            List<Vector> result = new List<Vector>();
+            foreach (string str in processText)
+            {
+                result.Add(VectoriseStandardInput(str, baseFile));
+            }
+            return result;
+        }
+    }
+}
